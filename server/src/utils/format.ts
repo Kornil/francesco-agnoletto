a79b27952f38cf4822a75b05e83aa564385d6513
Francesco Agnoletto <francesco.agnoletto92@gmail.com>
2026-06-06 03:08:18 +0300

    Trigger builds

--- server/src/utils/format.ts
+++ server/src/utils/format.ts
@@ -1,5 +1,6 @@
 export const sumAllValues = (values: number[]) =>
   values.reduce((sum, value) => sum + value, 0);
+
 export const averageAllValues = (values: number[]) =>
   values.reduce((sum, value) => sum + value, 0) / values.length;
 
