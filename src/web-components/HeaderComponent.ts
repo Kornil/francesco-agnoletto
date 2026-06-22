932cdd171fadd6bdb39b1c0ce1a9df20c098c275
Francesco Agnoletto <francesco.agnoletto92@gmail.com>
2026-06-22 23:03:59 +0300

    Fix linkedin url

--- src/web-components/HeaderComponent.ts
+++ src/web-components/HeaderComponent.ts
@@ -12,7 +12,7 @@ class HeaderComponent extends HTMLElement {
             <a href="https://github.com/Kornil" target="_blank" rel="noopener noreferrer" aria-label="Visit my GitHub profile">github</a>
           </li>
           <li>
-            <a href="https://linkedin.com/in/in/francesco-agnoletto-176171114/" target="_blank" rel="noopener noreferrer" aria-label="Visit my LinkedIn profile">linkedin</a>
+            <a href="https://linkedin.com/in/francesco-agnoletto-176171114/" target="_blank" rel="noopener noreferrer" aria-label="Visit my LinkedIn profile">linkedin</a>
           </li>
         </ul>
       </header>
